ad00c46a18500429b80abb05e27df2d81129a627
Yuanhua Huang <huangyh094@gmail.com>
2023-07-13 14:20:09 +0800

    minor updates

--- ctype_composition/csf_cell_type_composition.R
+++ ctype_composition/csf_cell_type_composition.R
@@ -4,12 +4,15 @@
 ##   - aod: https://www.rdocumentation.org/packages/aod/versions/1.3.1/topics/betabin
 ## remotes::install_github("holab-hku/DCATS", build_vignettes = FALSE)
 
+## Run this script:
+## /storage/yhhuang/systems/condaEnv/r_4.3/bin/Rscript fig1d-csf_cell_type_composition.R
+
 library(DCATS)
 library(ggplot2)
 
 ## Load data
-fig_dir  <- "/Users/yuanhua/research/msclerosis/data/celltype/figures/"
-anno_file <- "/Users/yuanhua/Downloads/expr_csf_annot_clean_rc3_obs.csv"
+fig_dir  <- "/usersdata/yuanhua/msNewData/figures/"
+anno_file <- "/usersdata/yuanhua/msNewData/data_release/expr_csf_annot_clean_rc3_obs.csv"
 
 df_ms <- read.table(anno_file, sep = ",", header = TRUE)
 df_ms[["celltype2"]] = df_ms$celltype
@@ -63,7 +66,7 @@ factor_pool <- factor_pool[, c('MS_vs_rest', 'MS_vs_IIH', 'MS_vs_NIND',
 
 factor2 <- cbind(df_donor$oligoclonal, df_donor$active, 
                  df_donor$treated, df_donor$gender - 1)
-colnames(factor2) <- c('oligoclonal', 'active', 'treated', 'gender')
+colnames(factor2) <- c('oligoclonal', 'active', 'treated', 'sex')
 
 factor_pool <- cbind(factor_pool, factor2)
 
@@ -75,7 +78,7 @@ res_beta <- DCATS::dcats_GLM(count_mat = cell_count, design_mat = factor_pool)
 
 
 ### Plot results
-source('~/MyGit/MSclerosisSrc/ctype_composition/dcats_plottings.R')
+source('../ctype_composition/dcats_plottings.R')
 matrix_dotplot(size_mat = -log10(res_beta$fdr), color_mat = res_beta$ceoffs,
                size_title = '-log10(FDR)', color_title = 'Effect size', 
                color_limits=c(-2,2)) +
@@ -85,3 +88,27 @@ matrix_dotplot(size_mat = -log10(res_beta$fdr), color_mat = res_beta$ceoffs,
 ggsave(filename = paste0(fig_dir, ct_level, ".rc3-DCATS", ".pdf"),
        width = 6, height = 6, dpi = 300)
 
+
+### Generate volcano plot
+prop_ctrl = colMeans(cell_count[factor_pool[,1]==0,] / 
+                     rowSums(cell_count[factor_pool[,1]==0,]))
+res_df = data.frame(
+    names=rownames(res_beta$LRT_pvals),
+    prop_ctrl=prop_ctrl, 
+    pval=res_beta$LRT_pvals[, 1], 
+    effsize=res_beta$ceoffs[, 1], 
+    effsize_err=res_beta$coeffs_err[,1]
+)
+
+library(ggplot2)
+ggplot(res_df, aes(x = effsize, y=-log10(pval), label=names, color=names)) +
+  geom_point(aes(size = prop_ctrl)) +
+  ggrepel::geom_text_repel(size = 2.5) +
+  geom_errorbarh(aes(xmin=effsize-effsize_err, 
+                     xmax=effsize+effsize_err, height = .0)) +
+  geom_hline(yintercept=-log10(0.05), color='red', linewidth=0.3) + 
+  geom_vline(xintercept=0, linewidth=0.3) +
+  xlab("MS effect size, logit scale") + guides(color="none") + xlim(-1.7, 2.3)
+
+ggsave(filename = paste0(fig_dir, ct_level, ".rc3-DCATS-vocalno", ".pdf"),
+       width = 6, height = 5, dpi = 300)
